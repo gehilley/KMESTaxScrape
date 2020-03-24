1cf85049b6d2a5fbd532107fbe44f61197ac3263
George Hilley <gehilley@gmail.com>
2020-03-24 06:10:12 -0700

    reorg

--- taxfunding.py
+++ taxfunding.py
@@ -1,5 +1,6 @@
-incremental_factors_file = '2019_2020_IncrementalFactorsList.csv'
-tax_pickle = 'kmes_taxes.p'
+incremental_factors_file = '../2019_2020_IncrementalFactorsList.csv'
+tax_pickle_for_apns = 'kmes_taxes.p'
+tax_history_pickle = '../cusd_1percent_tax_history.p'
 distribution_pickle_out = 'kmes_distribution.p'
 cabrillo_key = 50200
 
@@ -21,27 +22,45 @@ def read_incremental_factors():
 
 increment_map, funding_code_map = read_incremental_factors()
 import pickle as p
-tax_data = p.load(open(tax_pickle,'rb'))
-tax_distribution = dict()
-for tax_datum in tax_data:
-    apn = tax_datum[0]
-    this_distribution = tax_distribution.get(apn, {})
-    total_tax = tax_datum[2]
-    tra = tax_datum[1]
-    try:
-        for key in increment_map[tra].keys():
-            this_distribution[key] = increment_map[tra][key] * total_tax
-    except:
-        print('observed Null TRA.')
+tax_data_apns = p.load(open(tax_pickle_for_apns,'rb'))
+apns = list(set([d[0] for d in tax_data_apns]))
+apns.sort()
+tax_distribution = list()
+tax_history = p.load(open(tax_history_pickle,'rb'))
+tax_history_apns = [d[0] for d in tax_history]
 
-    tax_distribution[apn] = this_distribution
+for apn in apns:
+    try:
+        tax_history_index = tax_history_apns.index(apn)
+    except:
+        tax_history_index = None
+    if tax_history_index is None:
+        print('No Matching APN: ' + apn)
+    else:
+        this_tax_history = tax_history[tax_history_index]
+        total_tax = this_tax_history[3]
+        tra = this_tax_history[1]
+        this_tra = increment_map.get(tra, None)
+        if this_tra is None:
+            print('TRA is Null for APN: ' + apn)
+        else:
+            fraction = this_tra.get(cabrillo_key, None)
+            if fraction is None:
+                print('APN: ' + apn + ' is not in district')
+            else:
+                tax_distribution += [[this_tax_history[0], this_tax_history[1], this_tax_history[2], fraction, this_tax_history[3], [t*fraction for t in this_tax_history[3]]]]
 
 import numpy as np
 
-cabrillo_total = np.sum(np.array([tax_distribution[x].get(cabrillo_key) for x in tax_distribution if
-                                  tax_distribution[x].get(cabrillo_key) is not None]))
+district_data = np.array(np.array([x[5] for x in tax_distribution]))
 
-print('CUSD contributions from KMES boundary: ' + str(cabrillo_total))
+print('District Contributions: ')
+
+district_sum = np.sum(district_data, axis=0)
+year = 2007
+for ds in district_sum:
+    print(str(year) + ": " + str(ds))
+    year += 1
 
 p.dump([tax_distribution, funding_code_map], open(distribution_pickle_out,'wb'))
 
